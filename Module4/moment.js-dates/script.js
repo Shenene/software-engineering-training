a4e0bfff9eff0b2f40ffa9a0946147feff88d042
Shenene Carstens <shenene.carstens@gmail.com>
2026-06-21 14:25:39 +1200

    Completed Lab #8 - Dates(Luxon, Moment.js)

---
+++ Module4/moment.js-dates/script.js
@@ -0,0 +1,88 @@
+"use strict";
+
+// Using / Accessing:
+const output = document.getElementById("output");
+
+// ---------------------------------------------- //
+
+// My Birthdate
+const birthDate = moment("1980-07-10");
+
+// Current date & time
+const currentDate = moment();
+
+// ----------------
+
+// 1. Number of days between my birthdate and the current date
+const daysBetween = currentDate.diff(birthDate, "days");
+
+// ----------------------------------------------
+//
+
+// 2. Number of years, months and days between my birthdate and current date
+
+// Full year
+const years = currentDate.diff(birthDate, "years");
+
+// Years added to birthdate
+const birthDateAndYears = birthDate.clone().add(years, "years");
+
+// Remaining months
+const months = currentDate.diff(birthDateAndYears, "months");
+
+// Months added
+const birthDateAndYearsAndMonths = birthDateAndYears.clone().add(months, "months");
+
+// Remaining days
+const days = currentDate.diff(birthDateAndYearsAndMonths, "days");
+
+// ----------------------------------------------
+//
+
+// 3. Given two dates, display the date closest to the current date
+const date1 = moment("2026-02-05");
+const date2 = moment("2026-09-15");
+
+const differenceFromDate1 = Math.abs(currentDate.diff(date1, "days"));
+const differenceFromDate2 = Math.abs(currentDate.diff(date2, "days"));
+
+let closestDate;
+
+if (differenceFromDate1 < differenceFromDate2) {
+  closestDate = date1;
+} else {
+  closestDate = date2;
+}
+
+// ----------------------------------------------
+//
+
+// 4. Given two dates, display whether the first date is before or after the second date
+let beforeOrAfter;
+
+if (date1.isBefore(date2)) {
+  beforeOrAfter = "Date 1 is before Date 2";
+} else if (date1.isAfter(date2)) {
+  beforeOrAfter = "Date 1 is after Date 2";
+} else {
+  beforeOrAfter = "Date 1 is the same as Date 2";
+}
+
+// ----------------------------------------------
+//
+
+// 5. Display the current time in London
+const londonTime = moment().tz("Europe/London").format("HH:mm:ss");
+
+//
+// ----------------------------------------------
+//
+
+// Display on page
+output.innerHTML = `<h2>Result</h2>
+
+<p><strong>1. </strong> Number of days between my birthdate and the current date:<br> ${daysBetween} days</p>
+<p><strong>2. </strong> Number of years, months and days between my birthdate and current date:<br> ${years} years, ${months} months and ${days} days</p>
+<p><strong>3. </strong> Given two dates, display the date closest to the current date:<br> ${closestDate.format("DD MMMM YYY")} </p>
+<p><strong>4. </strong> Given two dates, display whether the first date is before or after the second date:<br> ${beforeOrAfter}</p>
+<p><strong>5. </strong> Given two dates, display whether the first date is before or after the second date:<br> ${londonTime}</p>`;
