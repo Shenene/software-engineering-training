e05ad439c62b7d91852e7dbb0dedff1707253ff8
Shenene Carstens <shenene.carstens@gmail.com>
2026-07-05 18:41:58 +1200

    HTML and CSS added for this project

---
+++ Module5-backend/exercise3-frontend/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import calculatorRoutes from "./routes/calculatorRoutes.js";
+
+// ---------------------------------------- //
+
+const app = express();
+const port = 3000;
+
+// ----------------------------------------
+
+app.get("/", (req, res) => {
+  res.send("Calculator API is running. Try /calculator/add?num1=5&num2=4");
+});
+
+// ----------------------------------------
+
+// Use calculator routes
+app.use("/calculator", calculatorRoutes);
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Calculator server running at http://localhost:${port}`);
+});
