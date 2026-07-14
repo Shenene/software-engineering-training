bd9726b07746fa76546a4eaa9500a4df9dfb9349
Shenene Carstens <shenene.carstens@gmail.com>
2026-07-15 11:19:01 +1200

    Added sort, reverse, filter and reset controls - Completed Module 6 - Exercise 4

--- Module6-react/big-cats-controls/src/components/BigCats.jsx
+++ Module6-react/big-cats-controls/src/components/BigCats.jsx
@@ -1,4 +1,5 @@
 import SingleCat from "./SingleCat.jsx";
+import { useState } from "react";
 
 // ------------------- //
 
@@ -49,14 +50,73 @@ const cats = [
 
 // --------------------------------------------------------------------------------------------
 
+// ⁡⁣⁢⁢F u n c t i o n s⁡
+
 function BigCats() {
+  const [currentCats, setCurrentCats] = useState(cats);
+
+  // ------------------------------------
+
+  // Sort Cats
+  function handleSortCats() {
+    const sortedCats = [...currentCats].sort((catA, catB) => catA.name.localeCompare(catB.name));
+
+    setCurrentCats(sortedCats);
+  }
+
+  // ------------------------------------
+
+  // Reverse Cats List
+  function handleReverseCats() {
+    const reversedCats = [...currentCats].reverse();
+
+    setCurrentCats(reversedCats);
+  }
+
+  // ------------------------------------
+
+  // Filter Panthera Cats
+  function handleFilterPanthera() {
+    const pantheraCats = cats.filter((cat) => cat.latinName.startsWith("Panthera"));
+
+    setCurrentCats(pantheraCats);
+  }
+
+  // ------------------------------------
+
+  // Reset Cats List
+  function handleResetCats() {
+    setCurrentCats(cats);
+  }
+
+  // --------------------------------------------------------------------------------------------
+
   return (
+    // ⁡⁣⁢⁢B u t t o n s⁡
     <section className="big-cats">
       <h1>Big Cats</h1>
 
+      <div className="big-cats-controls">
+        <button type="button" onClick={handleSortCats}>
+          Sort Alphabetically
+        </button>
+
+        <button type="button" onClick={handleReverseCats}>
+          Reverse List
+        </button>
+
+        <button type="button" onClick={handleFilterPanthera}>
+          Show Panthera Cats
+        </button>
+
+        <button type="button" onClick={handleResetCats}>
+          Reset List
+        </button>
+      </div>
+
       {/* Creating a single cat component */}
       <div>
-        {cats.map((cat) => (
+        {currentCats.map((cat) => (
           <SingleCat key={cat.id} name={cat.name} latinName={cat.latinName} image={cat.image} />
         ))}
       </div>
