a558b65e8b6f61b836eaf3bf79e8650d953c48a0
Shenene Carstens <shenene.carstens@gmail.com>
2026-07-18 06:48:15 +1200

    Initial folder setup for Calculator

---
+++ Module6-react/calculator/src/App.jsx
@@ -0,0 +1,12 @@
+import Calculator from "./components/Calculator.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Calculator />
+    </main>
+  );
+}
+
+export default App;
---
+++ Module6-react/calculator/src/components/Calculator.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+// ------------------------------ //
+
+function Calculator() {}
+
+// ------------------------------ //
+
+export default Calculator;
